4621504a594e6d0e7a3a8565c6d64aac7d2dabe8
Dushimepaulette1 <p.dushime1@alustudent.com>
2025-03-28 13:12:52 +0200

    few changes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from sparse_matrix import CompressedMatrix
 
 if __name__ == "__main__":
     try:
+        # options available
         print("Choose an operation to perform:")
         print("1. Addition")
         print("2. Subtraction")
@@ -9,7 +10,7 @@ if __name__ == "__main__":
         user_choice = int(input("Enter the number of the operation (1/2/3): "))
        
         operations = ["", "addition", "subtraction", "multiplication"]
-        
+        # option if the user choses any of the options
         if user_choice == 1:
             matrix_a = CompressedMatrix('./sample_inputs/easy_sample_02_1.txt')
             matrix_b = CompressedMatrix('./sample_inputs/easy_sample_02_2.txt')
@@ -22,12 +23,15 @@ if __name__ == "__main__":
             matrix_a = CompressedMatrix('./sample_inputs/easy_sample_02_1.txt')
             matrix_b = CompressedMatrix('./sample_inputs/easy_sample_02_3.txt')
             outcome = matrix_a.multiply(matrix_b)
+            # Raising an error incase a choice doesnot exist
         else:
             raise ValueError("Invalid selection")
         
         output_path = f'./results/result_{operations[user_choice]}.txt'
+        # printing where the file is saved 
         print("Saving file to : ", output_path)
         with open(output_path, 'w+') as file:
+    # writes the output to a file at a specified path
             file.write(str(outcome))
         
     except Exception as error:
